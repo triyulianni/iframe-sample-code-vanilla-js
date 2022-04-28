0ea97b7932b2d5a8886e77ccd640b2afeb311352
bao <baodtg@belive.sg>
2022-04-28 12:08:47 +0700

    🎉 [IF-191] initialize iframe sample code vanilla js

---
+++ main.js
@@ -0,0 +1,41 @@
+const app = document.querySelector("#app");
+
+app.innerHTML = `
+  <h1>Demo BeLive Player Widget!</h1>
+  <a href="/about.html">Go to another page</a>
+`;
+
+const BelivePlayerWidget = window.BeLivePlayerWidget;
+
+const player = BelivePlayerWidget.initialize({
+	showId: "a0832a15-b962-401e-9f95-5ea635d6373a",
+	triggerElement: document.getElementById("watch-button"),
+});
+
+player.on(BelivePlayerWidget.PlayerEventType.READY, () => {
+	console.log("Player Widget Ready");
+});
+
+player.on(BelivePlayerWidget.PlayerEventType.MINIMIZED, () => {
+	console.log("Player Widget MINIMIZED");
+});
+
+player.on(BelivePlayerWidget.PlayerEventType.UNMINIMIZED, () => {
+	console.log("Player Widget UNMINIMIZED");
+});
+
+player.on(BelivePlayerWidget.PlayerEventType.CLOSE, () => {
+	console.log("Player Widget CLOSE");
+});
+
+document.getElementById("close-button").addEventListener("click", () => {
+	player.close();
+});
+
+document.getElementById("minimize-button").addEventListener("click", () => {
+	player.minimize();
+});
+
+document.getElementById("unminimize-button").addEventListener("click", () => {
+	player.unminimize();
+});
